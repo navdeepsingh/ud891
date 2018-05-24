4fb5e1393c700efeadd43e66ef33ef1fcad2e5a8
Navdeep Singh <navdeep.er@gmail.com>
2018-05-24 14:14:40 +0530

    Update

--- lesson5-semantics-aria/03-first-steps/checkboxes.js
+++ lesson5-semantics-aria/03-first-steps/checkboxes.js
@@ -21,6 +21,12 @@
     this.el.addEventListener('click', this.toggle.bind(this));
 
     // Any other set-up we want to do here?
+    this.el.setAttribute('role', 'checkbox');
+    if (this.el.hasAttribute('checked')) {
+      this.el.setAttribute('aria-checked', 'true');
+    } else {
+      this.el.setAttribute('aria-checked', 'false');
+    }
   }
 
   Checkbox.prototype.handleKeyDown = function(e) {
@@ -36,12 +42,10 @@
   Checkbox.prototype.toggle = function() {
     if (this.el.hasAttribute('checked')) {
       this.el.removeAttribute('checked');
-
-      // Hmm.
-
+      this.el.setAttribute('aria-checked', 'false');
     } else {
       this.el.setAttribute('checked', '');
-
+      this.el.setAttribute('aria-checked', 'true');
       // Hmmmmm.
 
     }
